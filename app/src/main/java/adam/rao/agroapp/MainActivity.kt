6c1671f9efa15cd7a72ec2fce12fd6b58f193613
Adam Rao <raoadam87@gmail.com>
2019-09-03 11:43:18 +0300

    Adds layout where user can input land size and amount of seed planted

--- app/src/main/java/adam/rao/agroapp/MainActivity.kt
+++ app/src/main/java/adam/rao/agroapp/MainActivity.kt
@@ -2,11 +2,17 @@ package adam.rao.agroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seedInput = findViewById<TextInputEditText>(R.id.seed_input)
+        val landSizeInput = findViewById<TextInputEditText>(R.id.land_size_input)
+        val submitBtn = findViewById<MaterialButton>(R.id.btnSubmit)
     }
 }
