927729868a53e6f7d9b6c2cac1ee1d7918a4b0dd
Adam Rao <raoadam87@gmail.com>
2019-09-10 11:37:54 +0300

    Adds password reset and email reset methods

--- app/src/main/java/adam/rao/agroapp/utils/AuthUtils.kt
+++ app/src/main/java/adam/rao/agroapp/utils/AuthUtils.kt
@@ -5,7 +5,9 @@ import adam.rao.agroapp.SignInActivity
 import adam.rao.agroapp.SignUpActivity
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 
 fun checkEmailAndPasswordNotEmpty(context: Context, email: String, password: String): Boolean {
@@ -49,4 +51,38 @@ fun userHasAccountButEmailNotVerified(context: Context) {
     val intent = Intent(context, SignInActivity::class.java)
     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     context.startActivity(intent)
+}
+
+fun resetPassword(email: String, context: Context) {
+    FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener { task ->
+        if(task.isSuccessful) {
+            Toast.makeText(context, "Password Reset Link sent to Email", Toast.LENGTH_LONG).show()
+            FirebaseAuth.getInstance().signOut()
+        }
+    }
+}
+
+fun resetEmailAddress(email: String, password: String, context: Context) {
+    val credentials = EmailAuthProvider.getCredential(FirebaseAuth.getInstance()
+        .currentUser!!.email as String, password)
+    FirebaseAuth.getInstance().currentUser!!.reauthenticate(credentials).addOnCompleteListener { task ->
+        if(task.isSuccessful) {
+            FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email).addOnCompleteListener { task1 ->
+                if(task1.result!!.signInMethods!!.size == 1) {
+                    Toast.makeText(context, "Email Already Exists, choose another", Toast.LENGTH_LONG).show()
+                } else {
+                    FirebaseAuth.getInstance().currentUser!!.updateEmail(email).addOnCompleteListener { task2 ->
+                        if(task2.isSuccessful) {
+                            Toast.makeText(context, "Email Reset Successful", Toast.LENGTH_LONG).show()
+                            FirebaseAuth.getInstance().currentUser!!.sendEmailVerification()
+                            FirebaseAuth.getInstance().signOut()
+                        }
+                    }
+                }
+            }
+        }
+    }.addOnFailureListener { e ->
+        Toast.makeText(context, "Failed to Re-authenticate", Toast.LENGTH_LONG).show()
+        Log.d("TAG", e.message)
+    }
 }
